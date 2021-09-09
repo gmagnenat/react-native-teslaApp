ac7b51cb4383b91f7972cd134698c58cbe700128
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-09-09 18:42:16 +0200

    Create a StyledButton component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import { Text, View, ImageBackground } from 'react-native'
+import StyledButton from '../StyledButton'
 import styles from './styles'
 
 const CarItem = (props) => {
   return (
     <View style={styles.carContainer}>
         <ImageBackground source={require('../../assets/images/ModelS.jpeg')} style={styles.image} />
+
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subTitle}>Starting at $69,420</Text>
         </View>
+
+        <StyledButton type="primary" content="Custom Order" onPress={() => {console.warn("Custom Order was pressed")}} />
       </View>
   )
 }
